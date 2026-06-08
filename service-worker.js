1d61f96c795e6987a5c7c0058e803bbb4e576988
AlbertoBlestGana <hsaidoku@gmail.com>
2026-06-08 16:26:04 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-const CACHE="qr-registro-v4"
+const CACHE="qr-registro-v5"
 
 const ASSETS=[
 "./",
 "./index.html",
 "./app.js",
 "./manifest.json",
-"./logo.png"
+"./logo.png",
+"./icon-192.png",
+"./icon-512.png"
 ]
 
 self.addEventListener("install",e=>{
 
 e.waitUntil(
-caches.open(CACHE).then(cache=>{
-return cache.addAll(ASSETS)
-})
+
+caches.open(CACHE)
+.then(cache=>cache.addAll(ASSETS))
+
 )
 
 self.skipWaiting()
@@ -23,15 +26,25 @@ self.skipWaiting()
 self.addEventListener("activate",e=>{
 
 e.waitUntil(
+
 caches.keys().then(keys=>{
+
 return Promise.all(
+
 keys.map(key=>{
+
 if(key!==CACHE){
+
 return caches.delete(key)
+
 }
+
 })
+
 )
+
 })
+
 )
 
 self.clients.claim()
@@ -41,9 +54,14 @@ self.clients.claim()
 self.addEventListener("fetch",e=>{
 
 e.respondWith(
-caches.match(e.request).then(res=>{
-return res || fetch(e.request)
+
+caches.match(e.request)
+.then(response=>{
+
+return response || fetch(e.request)
+
 })
+
 )
 
 })
